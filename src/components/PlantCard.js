9e5fbc08334f16979677c458b251f2fb2257f9b5
Jasec <jasec_7@hotmail.com>
2025-06-22 23:31:10 -0700

    complete react hooks cc plantshop

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,15 +1,15 @@
 import React from "react";
 
-function PlantCard() {
+function PlantCard({id, name, image, price, onClick, isOutOfStock}) {
   return (
     <li className="card" data-testid="plant-item">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+      <img src={image} alt={name} />
+      <h4>{name}</h4>
+      <p>Price: {price}</p>
+      {!isOutOfStock ? (
+        <button className="primary" onClick={() => onClick(id)}>In Stock</button>
       ) : (
-        <button>Out of Stock</button>
+        <button onClick={() => onClick(id)}>Out of Stock</button>
       )}
     </li>
   );
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,43 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+  const [isSoldOut, setIsSoldOut] = useState([]);
+  const [searchPlant, setSearchPlant] = useState("")
+
+  function handleSold(id) {
+    setIsSoldOut((prev) =>
+    prev.includes(id)
+      ? prev.filter((plantId) => plantId !== id) 
+      : [...prev, id] 
+  );
+};
+function handleSearch(e){
+  setSearchPlant(e.target.value)
+};
+
+const filterPlants = plants.filter((plant) => {
+  return plant.name.toLowerCase().includes(searchPlant.toLowerCase())
+})
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+    .then((r) => r.json())
+    .then((plants) => setPlants(plants))
+  },[]);
+
+  function handleAddPlant(newPlant){
+    setPlants([...plants, newPlant])
+  };
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={handleAddPlant} />
+      <Search onSearch={handleSearch}/>
+      <PlantList plants={filterPlants} onClickSold={handleSold} isSoldOut={isSoldOut}/>
     </main>
   );
 }
